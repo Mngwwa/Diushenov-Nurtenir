b5ed911c9ee509fe6a34dd9ff7c098625831e8d2
Diushenov Nurtenir draft <dn12842@auca.kg>
2024-12-19 17:13:25 -0800

    first ii

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace std;
 int main() {
-    int number [10];
+    int number [10];l
 
     cout << "Enter 10 integers for the array:" << endl;
     for (int i = 0; i < 10; i++) {
